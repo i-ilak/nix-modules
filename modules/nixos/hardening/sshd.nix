8f54d7bf01a0ee955aad184ebdf308a0b7e3ce09
Ivan Ilak <ivan.ilak@hotmail.com>
2026-05-20 11:05:39 +0200

    feat: parameterize hardening/sshd.nix with infra.profiles.sshd

--- modules/nixos/hardening/sshd.nix
+++ modules/nixos/hardening/sshd.nix
@@ -1,31 +1,47 @@
-_: {
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitEmptyPasswords = false;
-      PermitTunnel = false;
-      PermitRootLogin = "no";
-      UseDns = false;
-      KbdInteractiveAuthentication = false;
-      AllowTcpForwarding = true;
-      X11Forwarding = false;
-      MaxAuthTries = 3;
-      MaxSessions = 2;
-      ClientAliveInterval = 300;
-      ClientAliveCountMax = 0;
-      AllowAgentForwarding = false;
-      AllowStreamLocalForwarding = false;
-      AllowUsers = [ ];
-      TCPKeepAlive = false;
-      LogLevel = "VERBOSE";
+{ config, lib, ... }:
+{
+  options.infra.profiles.sshd = {
+    allowUsers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "root" ];
+      description = "Users permitted to SSH in. Overrides hardening default.";
     };
-    ports = [ 22023 ];
-    hostKeys = [
-      {
-        type = "ed25519";
-        path = "/etc/ssh/ssh_host_ed25519_key";
-      }
-    ];
+    ports = lib.mkOption {
+      type = lib.types.listOf lib.types.port;
+      default = [ 22023 ];
+      description = "SSH listen ports.";
+    };
+  };
+
+  config = {
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitEmptyPasswords = false;
+        PermitTunnel = false;
+        PermitRootLogin = "no";
+        UseDns = false;
+        KbdInteractiveAuthentication = false;
+        AllowTcpForwarding = true;
+        X11Forwarding = false;
+        MaxAuthTries = 3;
+        MaxSessions = 2;
+        ClientAliveInterval = 300;
+        ClientAliveCountMax = 0;
+        AllowAgentForwarding = false;
+        AllowStreamLocalForwarding = false;
+        AllowUsers = lib.mkForce config.infra.profiles.sshd.allowUsers;
+        TCPKeepAlive = false;
+        LogLevel = "VERBOSE";
+      };
+      ports = config.infra.profiles.sshd.ports;
+      hostKeys = [
+        {
+          type = "ed25519";
+          path = "/etc/ssh/ssh_host_ed25519_key";
+        }
+      ];
+    };
   };
 }
